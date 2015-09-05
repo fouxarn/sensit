94382b4924290b62ad3ee3e9e9d6d7c37e392b36
Daniel Nilsson <danni701@student.liu.se>
2015-09-05 14:38:11 +0200

    fixt paringdata

--- PakeringsData.py
+++ PakeringsData.py
@@ -1,5 +1,7 @@
 import json, requests, pprint
 key = '66fc20e245084815af770af82d2ceef6'
+pp = pprint.PrettyPrinter(indent=4)
+
 
 def getPakeringLkpgdata(timeString):
 	url = 'http://parkering.linkoping.se/Parkeringsdata/ParkeringsdataV1.svc/GetParkeringsytaList/66fc20e245084815af770af82d2ceef6/080000'
@@ -7,7 +9,16 @@ def getPakeringLkpgdata(timeString):
 	resp = requests.get(url=url)
 	data = json.loads(resp.text)
 	pp = pprint.PrettyPrinter(indent=4)
-	pp.pprint(data)
+	#pp.pprint(data)
+	dataResult =data['ParkingAreaNewList']
+	returnData = []
+	for fields in dataResult:
+		
+
+		if(fields['Latitude']<58.4119 and fields['Latitude']>58.4087 and fields['Longitude']<15.6249 and fields['Longitude']>15.6089):
+			returnData.append(fields)
+	return returnData
+	
 
 
-getPakeringLkpgdata("timeString")
+pp.pprint(getPakeringLkpgdata("timeString"))
